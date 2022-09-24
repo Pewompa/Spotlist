015d76ebeaa85417e91b5e346080cef4a47a8e5a
Bernat <bernatpavon@gmail.com>
2022-09-24 11:18:08 +0200

    add: db, usermodel and router

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const connectDB = require('./config/db');
+// const router = require('./router/listRouter');
 const cors = require('cors');
+connectDB();
 
 app.use(cors());
 app.use(express.json());
+// app.use(router);
 
 app.listen(port, () => {
   console.log(`app started on port ${port}`);
